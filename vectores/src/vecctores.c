75de24c37d22ccd8f3b9d8adaa82014cde848bc0
PILAR <PILAR@DESKTOP-ISL2DJ6>
2023-11-09 13:59:23 +0100

    commit central

---
+++ vectores/src/vecctores.c
@@ -0,0 +1,8 @@
+/*
+ * vecctores.c
+ *
+ *  Created on: 9 nov 2023
+ *      Author: PILAR
+ */
+
+
---
+++ vectores/src/vecctores.h
@@ -0,0 +1,15 @@
+/*
+ * vecctores.h
+ *
+ *  Created on: 9 nov 2023
+ *      Author: PILAR
+ */
+
+#ifndef VECCTORES_H_
+#define VECCTORES_H_
+
+void posicionprimerelemento(int tam, int vector[]);
+void mostrarvectoresmenoresque(int tam, int vector);
+
+
+#endif /* VECCTORES_H_ */
